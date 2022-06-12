233235ba2cdc668f8f0bcb0dfed7358e3bab46ee
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-06-12 17:43:33 +0900

    [Bronze V] Title: 긴자리 계산, Time: 144 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(BigInt);
+
+const A = input[0];
+const B = input[1];
+
+console.log(`${A + B}
+${A - B}
+${A * B}`);
