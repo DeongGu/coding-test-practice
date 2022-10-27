ea4b2cfd016ca2a21a422715339b88b560a5c817
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-10-27 23:13:25 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,7 +1,9 @@
 def solution(citations):
     citations.sort(reverse = True)
     for i in range(len(citations)):
-        if i + 1 >= citations[i]:
+        if i + 1 > citations[i]:
             return i
+        elif i + 1 == citations[i]:
+            return citations[i]
     
     return len(citations)
