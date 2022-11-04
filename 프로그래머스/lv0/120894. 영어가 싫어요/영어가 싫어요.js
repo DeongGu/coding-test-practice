a771d415e441de4e7f8f62ebfed4634a5ca4fffa
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-11-04 22:56:17 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,18 @@
+function solution(numbers) {
+   let result = numbers;
+    const num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    
+ 
+   result = numbers.replace(/zero/g, "0");
+   result = result.replace(/one/g, "1");
+   result = result.replace(/two/g, "2");
+   result = result.replace(/three/g, "3");
+   result = result.replace(/four/g, "4");
+   result = result.replace(/five/g, "5");
+   result = result.replace(/six/g, "6");
+   result = result.replace(/seven/g, "7");
+   result = result.replace(/eight/g, "8");
+   result = result.replace(/nine/g, "9");
+    
+     return +result;
+}
