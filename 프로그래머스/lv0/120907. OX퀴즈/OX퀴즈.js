121b070c574778ca61727b7cb090ba1725712b17
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-11-06 02:37:45 +0900

    [level 0] Title: OX퀴즈, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -2,8 +2,12 @@ function solution(quiz) {
     const result = [];
     
    const new_quiz = quiz.map((el)=> el.replace("=","==="));
-    new_quiz.map((el)=> eval(el) ? result.push("O"): result.push("X"));
+//     new_quiz.map((el)=> eval(el) ? result.push("O"): result.push("X"));
     
+//     return result;
+    
+   new_quiz.map((el) => Function('return '+el)() ? result.push("O"): result.push("X"));
+   
     return result;
     
 }
