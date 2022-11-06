e5091c5105a9cb4ecdf804f82b0b5c135fb6d93d
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-11-06 23:43:56 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -1,48 +1,72 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
 function solution(n) {
-    const result = {};
-    let num = 1;
-    for(let i = 1; i <= 100; i++){
-        
-       
-        
-        if(num%3 === 0 || num.toString().split("").includes("3")){
-            num++;
-            if(num%3 === 0 || num.toString().split("").includes("3")){
-                num++;
-                if(num%3 === 0 || num.toString().split("").includes("3")){
-                     num++;
-                    if(num%3 === 0 || num.toString().split("").includes("3")){
-                        num++;
-                        if(num%3 === 0 || num.toString().split("").includes("3")){
-                            num++;
-                        }
-                    }
-                }
-            }
-        }
-        
-         result[i] = num;  
-        
-        num++;
-        if(num%3 === 0 || num.toString().split("").includes("3")){
-            num++;
-            if(num%3 === 0 || num.toString().split("").includes("3")){
-                num++;
-                if(num%3 === 0 || num.toString().split("").includes("3")){
-                     num++;
-                    if(num%3 === 0 || num.toString().split("").includes("3")){
-                        num++;
-                        if(num%3 === 0 || num.toString().split("").includes("3")){
-                            num++;
-                            if(num%3 === 0 || num.toString().split("").includes("3")){
-                                num++;
-                             }
-                        }
-                    }
-                }
+    let cnt = 0;
+    for(let i = 1; i < 200; i++) {
+        if(i%3 != 0 && !i.toString().split('').includes('3')) {
+            cnt++;
+            if(cnt == n) {
+                return i;
             }
         }
     }
-    
-    return result[n];
 }
+
+// function solution(n) {
+//     const result = {};
+//     let num = 1;
+//     for(let i = 1; i <= 100; i++){
+        
+       
+        
+//         if(num%3 === 0 || num.toString().split("").includes("3")){
+//             num++;
+//             if(num%3 === 0 || num.toString().split("").includes("3")){
+//                 num++;
+//                 if(num%3 === 0 || num.toString().split("").includes("3")){
+//                      num++;
+//                     if(num%3 === 0 || num.toString().split("").includes("3")){
+//                         num++;
+//                         if(num%3 === 0 || num.toString().split("").includes("3")){
+//                             num++;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+        
+//          result[i] = num;  
+        
+//         num++;
+//         if(num%3 === 0 || num.toString().split("").includes("3")){
+//             num++;
+//             if(num%3 === 0 || num.toString().split("").includes("3")){
+//                 num++;
+//                 if(num%3 === 0 || num.toString().split("").includes("3")){
+//                      num++;
+//                     if(num%3 === 0 || num.toString().split("").includes("3")){
+//                         num++;
+//                         if(num%3 === 0 || num.toString().split("").includes("3")){
+//                             num++;
+//                             if(num%3 === 0 || num.toString().split("").includes("3")){
+//                                 num++;
+//                              }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+    
+//     return result[n];
+// }
