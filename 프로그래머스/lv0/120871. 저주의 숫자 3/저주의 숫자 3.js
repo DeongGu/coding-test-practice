3a878e444c33917be0400f2b9b522fe1e80457f8
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-11-06 23:32:54 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,75 @@
+function solution(n) {
+    const result = {};
+    let num = 1;
+    for(let i = 1; i <= 100; i++){
+        
+        // if(i%3 === 0 || i.toString().split("").includes("3")){
+        //     num++;
+        // }
+        
+       
+        
+        if(num%3 === 0 || num.toString().split("").includes("3")){
+            num++;
+            if(num%3 === 0 || num.toString().split("").includes("3")){
+                num++;
+                if(num%3 === 0 || num.toString().split("").includes("3")){
+                     num++;
+                    if(num%3 === 0 || num.toString().split("").includes("3")){
+                        num++;
+                        if(num%3 === 0 || num.toString().split("").includes("3")){
+            num++;
+            if(num%3 === 0 || num.toString().split("").includes("3")){
+                num++;
+                if(num%3 === 0 || num.toString().split("").includes("3")){
+                     num++;
+                    if(num%3 === 0 || num.toString().split("").includes("3")){
+                        num++;
+                    }
+                }
+            }
+        }
+                    }
+                }
+            }
+        }
+        
+         result[i] = num;  
+        
+        num++;
+        if(num%3 === 0 || num.toString().split("").includes("3")){
+            num++;
+            if(num%3 === 0 || num.toString().split("").includes("3")){
+                num++;
+                if(num%3 === 0 || num.toString().split("").includes("3")){
+                     num++;
+                    if(num%3 === 0 || num.toString().split("").includes("3")){
+                        num++;
+                        if(num%3 === 0 || num.toString().split("").includes("3")){
+            num++;
+            if(num%3 === 0 || num.toString().split("").includes("3")){
+                num++;
+                if(num%3 === 0 || num.toString().split("").includes("3")){
+                     num++;
+                    if(num%3 === 0 || num.toString().split("").includes("3")){
+                        num++;
+                    }
+                }
+            }
+        }
+                    }
+                }
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    return result[n];
+}
