1578276d5234384bfd632a683440a4fc1e72c0c1
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-01-13 00:23:58 +0900

    [Silver III] Title: N과 M (1), Time: 224 ms, Memory: 18044 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const N = input[0];
+const M = input[1];
+
+let result = "";
+const answer = [];
+
+function repeatPermutation(x) {
+  if (x == M) {
+    result += `${answer.join(" ")}\n`;
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (answer.includes(i + 1)) {
+      continue;
+    }
+    answer.push(i + 1);
+    repeatPermutation(answer.length);
+    answer.pop();
+  }
+}
+
+repeatPermutation(0);
+
+console.log(result.trim());
