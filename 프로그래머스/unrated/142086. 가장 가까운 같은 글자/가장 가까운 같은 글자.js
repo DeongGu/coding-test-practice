85f3cd4df76907fbec7cd768d9b999b17d75fde4
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-03-06 12:03:26 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 6.85 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,7 +1,6 @@
 function solution(s) {
     const answer = [];
     const objStr = {};
-    const test =[];
     
     for(let i = 0; i<s.length; i++){
         if(!objStr[s[i]] && objStr[s[i]] !== 0){
@@ -9,7 +8,6 @@ function solution(s) {
             objStr[s[i]] = i;
         }else{
             answer.push(i - objStr[s[i]]);
-            test.push(objStr[s[i]]);
             objStr[s[i]] = i;
         }
     }
