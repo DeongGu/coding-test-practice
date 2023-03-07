4b63f6e285e34748ebaf9cf7e7ea581759ac1121
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-03-07 13:44:26 +0900

    [unrated] Title: 둘만의 암호, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,17 @@
+function solution(s, skip, index) {
+    const alpha =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    
+    const skipArr = [...skip];
+    
+    const newAlpha = alpha.filter(el => !skipArr.includes(el));
+    
+    const resultAlpha = [...newAlpha, ...newAlpha, ...newAlpha,...newAlpha];
+    
+    const result = [];
+    
+    s.split("").map((el,idx) => {
+        result.push(resultAlpha[newAlpha.indexOf(el)+index]);    
+    });
+    
+    return result.join("");
+}
