d3466e1aa58fee7bc8be4cc97741e0e03faf3c26
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-03-18 22:48:04 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 11.18 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,54 +1,36 @@
 function solution(m, n, board) {
-    const currentBoard = board.map(el => el.split(""));
-    let deleteCount =  0;
-    
+    let curBoard = [...board.map(block => [...block])].concat();
+    let newBoard = [...board.map(block => [...block])].concat();
 
-    
-    for(let l = 0; l <= m*n; l++){
-    const deleteBlock = [];
- 
-    for(let i = 0; i < m-1; i++){
-        for(let j = 0; j < n-1; j++){
-            if(currentBoard[i][j] === "") continue;
-            if(currentBoard[i][j] === currentBoard[i][j+1] && currentBoard[i][j] === currentBoard[i+1][j] && currentBoard[i][j] === currentBoard[i+1][j+1]){
-                
-                deleteBlock.push([i,j]);
-            }      
-        }  
-    }
-    
-    for(let i = 0; i < deleteBlock.length; i++){
-        const x = deleteBlock[i];
-        if(currentBoard[x[0]][x[1]] !== "") {
-            deleteCount+=1;
-            currentBoard[x[0]][x[1]] = "";
-        }
-        if(currentBoard[x[0]][x[1]+1] !== "") {
-            deleteCount+=1;
-            currentBoard[x[0]][x[1]+1] = "";
-        }
-        if(currentBoard[x[0]+1][x[1]] !== "") {
-            deleteCount+=1;
-            currentBoard[x[0]+1][x[1]] = "";
-        }
-        if(currentBoard[x[0]+1][x[1]+1] !== "") {
-            deleteCount+=1;
-            currentBoard[x[0]+1][x[1]+1] = "";
-        }
-           
-    }
-    for (let i = 0; i < m - 1; i++) {
-            for (let j = 0; j < n; j++) {
-                if (currentBoard[i + 1][j] === '') {
-                    for (let k = i; k >= 0; k--) {
-                        currentBoard[k + 1][j] = currentBoard[k][j];
-                        currentBoard[k][j] = '';
+    while (true) { 
+        let count = 0;
+        for (let x = 0; x < m - 1; x++) {
+            for (let y = 0; y < n - 1; y++) {
+                if (curBoard[x][y] === '') continue;
+                if (curBoard[x][y] === curBoard[x][y + 1] && curBoard[x][y] === curBoard[x + 1][y] && curBoard[x][y] === curBoard[x + 1][y + 1]) {
+                    newBoard[x][y] = '';
+                    newBoard[x + 1][y] = '';
+                    newBoard[x][y + 1] = '';
+                    newBoard[x + 1][y + 1] = '';
+                    count += 1;
                 }
             }
         }
+
+        for (let x = 0; x < m - 1; x++) {
+            for (let y = 0; y < n; y++) {
+                if (newBoard[x + 1][y] === '') {
+                    for (let i = x; i >= 0; i--) {
+                        newBoard[i + 1][y] = newBoard[i][y];
+                        newBoard[i][y] = '';
+                    }
+                }
+            }
+        }
+        curBoard = [...newBoard.map(block => [...block])].concat();
+        if (count === 0) break;
     }
-    }
-    
-        
-    return deleteCount;
+
+    const allBoards = newBoard.reduce((acc, cur) => acc.concat(cur));
+    return allBoards.filter(block => block === '').length;
 }
