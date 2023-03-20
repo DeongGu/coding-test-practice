9c00c759b76364391d491e3b7311b1b2534ff481
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-03-20 15:54:08 +0900

    [level 2] Title: 소수 찾기, Time: 4.51 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,21 +1,79 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
 function solution(numbers) {
-    const numbersArr = numbers.split("").sort((a,b) => b - a);
-   
-    const primesArr = Array(parseInt(numbersArr.join(""))+1).fill(true).fill(false, 0,2);
-    
-     for(let i = 2 ; i * i <= parseInt(numbersArr.join("")); i++){
-        if(primesArr[i]){
-            for(let j = i * i; j <= parseInt(numbersArr.join("")); j+=i){
-                primesArr[j] = false;
+    var answer = 0;
+
+    var n = numbers.split('');
+    var nums = new Set()
+    combi(n,'');
+
+    function combi(a, s) {
+        if (s.length > 0) {
+            if (nums.has(Number(s))=== false) {
+                nums.add(Number(s));
+            console.log(Number(s))
+                if (chkPrime(Number(s))) {
+
+                    answer++;
+                }
+            }
+        }
+        if (a.length > 0) {
+            for (var i = 0; i< a.length; i++) {
+                var t = a.slice(0)
+                t.splice(i,1);
+                //console.log(t)
+                combi(t,s + a[i]);
             }
         }
     }
-    
-    let primes = primesArr.map((el, idx) => el ? idx : 0).filter(el => el).map(String);
-    
-    for(let i = 0; i < numbersArr.length; i++){
-       primes = primes.map(el => el.replace(numbersArr[i],"")); 
+
+    function chkPrime(num) {
+        if (num < 2) return false;
+        if (num === 2) return true;
+        for (var i = 2; i <= Math.sqrt(num); i++) {
+            if (num%i===0) return false;
+        }
+        return true;
     }
-    
-    return primes.filter((el) => el ==="").length;
+
+    return answer;
 }
