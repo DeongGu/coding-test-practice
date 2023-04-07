afb226fdc63fe479edd398876e74f9ec84fd6b22
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-04-07 18:59:15 +0900

    [unrated] Title: 달리기 경주, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,24 +1,21 @@
 function solution(players, callings) {
-  // players의 순서를 저장할 객체 생성
-  const playerOrder = {};
-  for (let i = 0; i < players.length; i++) {
-    playerOrder[players[i]] = i;
-  }
-
-  // callings을 순회하면서 playerOrder 업데이트
-  for (let i = 0; i < callings.length; i++) {
-    const player = callings[i];
-    // 이미 1위인 경우 패스
-    if (playerOrder[player] === 0) continue;
-    // player의 바로 앞 선수의 이름을 찾아서 추월
-    const prevPlayer = players[playerOrder[player] - 1];
-    playerOrder[prevPlayer]++;
-    playerOrder[player]--;
-    // players 배열에서도 순서 변경
-    const temp = players[playerOrder[prevPlayer]];
-    players[playerOrder[prevPlayer]] = players[playerOrder[player]];
-    players[playerOrder[player]] = temp;
-  }
-
-  return players;
+    const objPlayers = {};
+    
+    players.forEach((el,idx) => objPlayers[el] = idx);
+    
+    callings.forEach((el,idx) => {
+        if(objPlayers[el] !== 0){
+            const player = el;
+            const frontPlayer = players[objPlayers[player]-1];
+            const temp = players[objPlayers[frontPlayer]];
+            objPlayers[player]--;
+            objPlayers[frontPlayer]++;
+            
+            players[objPlayers[player]] = player;
+            players[objPlayers[frontPlayer]] = temp;
+            
+        }       
+    })
+    
+    return players;
 }
