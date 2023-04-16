19e4a9907077a7940522538828cf8b97307da97c
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-04-16 18:27:38 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.js
@@ -0,0 +1,14 @@
+function solution(w, h) {
+    let result = BigInt(w*h);
+    const getCount = (x) => {
+        return (-1)*x*h/w + h
+    }
+    
+    for(let i = 1; i <= w; i++){
+        let count = Math.ceil(getCount(i-1)) - Math.floor(getCount(i));    
+        result-= BigInt(count);
+    }
+    
+    
+    return BigInt(result);
+}
