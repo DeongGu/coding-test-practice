d96fcc612653aaa4e0b1ac54d2a434ad9c916a61
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-04-25 09:52:51 +0900

    [unrated] Title: 배열 만들기 4, Time: 5.36 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,23 @@
+function solution(arr) {
+    const stk = [];
+    
+    let i = 0;
+    
+    while(i < arr.length){
+        if(stk.length === 0){
+            stk.push(arr[i]);
+            i+=1;
+        }
+        
+        if(stk.length > 0 && stk[stk.length-1] < arr[i]){
+            stk.push(arr[i]);
+            i+=1;
+        }
+        
+        if(stk.length > 0 && stk[stk.length-1] >= arr[i]){
+            stk.pop();
+        }
+    }
+    
+    return stk;
+}
