8d142877bc38cf47b183a495294c504807f7b771
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2023-04-25 18:09:31 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 5.65 ms, Memory: 44.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    return arr.map((el) => {
+        if(el >= 50 && el % 2 === 0){
+            return el/2;
+        }
+        if(el < 50 && el & 2 !== 0){
+            return el*2;
+        }
+        
+        return el;
+    })
+}
