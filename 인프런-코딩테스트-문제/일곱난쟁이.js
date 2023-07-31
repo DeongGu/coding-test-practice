cd2a3ddc55329e33826bbd6d699ad27e6f540ca1
SeongWoo Shin <ssw4510@gmail.com>
2023-07-31 12:44:16 +0900

    예외 사항으로 인한 코드 수정 (여러 경우의 수 제거)

--- 인프런-코딩테스트-문제/일곱난쟁이.js
+++ 인프런-코딩테스트-문제/일곱난쟁이.js
@@ -1,6 +1,6 @@
 function solution(arr) {
   let sum = arr.reduce((a, b) => a + b, 0);
-
+  let flag = 0;
   for (let i = 0; i < arr.length - 1; i++) {
     const first = arr[i];
     for (let j = i + 1; j < arr.length; j++) {
@@ -9,8 +9,11 @@ function solution(arr) {
       if (sum - first - second === 100) {
         arr.splice(j, 1);
         arr.splice(i, 1);
+        flag = 1;
+        break;
       }
     }
+    if (flag === 1) break;
   }
   return arr;
 }
