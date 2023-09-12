37d38c34815f02d30dcf38a6bb18d9313103287f
SeongWoo Shin <ssw4510@gmail.com>
2023-09-12 22:07:51 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.js
@@ -0,0 +1,27 @@
+function solution(operations) {
+    const newOperations = operations.map((el) => el.split(" "));
+    const arr = [];
+    
+    for(const [order, number] of newOperations){
+        if(order === "I") arr.push(+number);
+        if(order === "D" && number === "1") {
+            const max = Math.max(...arr);
+            const maxIndex = arr.indexOf(max);
+            arr.splice(maxIndex, 1);
+        }
+        
+        if(order === "D" && number === "-1"){
+            const min = Math.min(...arr);
+            const minIndex = arr.indexOf(min);
+            arr.splice(minIndex, 1);
+        }
+    }
+    
+    if(arr.length === 0 ) return [0,0];
+    else{
+        const max = Math.max(...arr);
+        const min = Math.min(...arr);
+        return [max, min];
+    }
+    
+}
