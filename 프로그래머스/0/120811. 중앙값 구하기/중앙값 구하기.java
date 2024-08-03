4bf5eda7226899775501d59d4cccce1b56ab47a2
SeongWoo Shin <ssw4510@gmail.com>
2024-08-03 17:07:06 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.27 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,8 +1,14 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] array) {
-        Arrays.sort(array);
+        for(int i = 0; i < array.length-1; i++){
+            for(int j = 0; j < array.length-1-i; j++){
+                if(array[j] > array[j+1]){
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }  
         
         return array[array.length/2];
     }
