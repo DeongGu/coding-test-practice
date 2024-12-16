9d6f723f12159e5116dcfda85397f8f10b2e9e3a
SeongWoo Shin <ssw4510@gmail.com>
2024-12-16 23:47:15 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 948 ms, Memory: 103792 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int[] arr;
+	static List<Integer>[] list;
+	static boolean[] visited;
+	static int count;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		String[] NMR = br.readLine().split(" ");
+		int N = Integer.parseInt(NMR[0]);
+		int M = Integer.parseInt(NMR[1]);
+		int R = Integer.parseInt(NMR[2]);
+		
+		arr = new int[N+1];
+		list = new ArrayList[N+1];
+		visited = new boolean[N+1];
+		count = 1;
+		
+		for(int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < M; i++) {
+			
+			 String[] uv = br.readLine().split(" ");
+			 int u = Integer.parseInt(uv[0]);
+			 int v = Integer.parseInt(uv[1]);
+			 
+			 list[u].add(v);
+			 list[v].add(u);
+			
+		}
+		
+		for (int i = 1; i <= N; i++) {
+            list[i].sort(Comparator.reverseOrder());
+        }
+		
+		dfs(R);
+		
+		
+		for(int i = 1; i <= N; i++) {
+			sb.append(arr[i]).append("\n");
+		}
+
+		bw.write(sb.toString().trim());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+	static void dfs(int v) {
+		
+		visited[v] = true;
+		arr[v] = count++;
+		
+		for(int w : list[v]) {
+			if(!visited[w]) {
+				dfs(w);
+			}
+		}
+		
+	}
+}
