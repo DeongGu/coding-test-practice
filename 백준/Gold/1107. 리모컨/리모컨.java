b2efe8523782956bb70f85c623f13fa34984900b
SeongWoo Shin <ssw4510@gmail.com>
2025-01-06 22:41:03 +0900

    [Gold V] Title: 리모컨, Time: 288 ms, Memory: 67328 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -1,49 +1,45 @@
 import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int result = Math.abs(N - 100);
 
-		int N = Integer.parseInt(br.readLine());
+        boolean[] btn = new boolean[10];
 
-		int result = Math.abs(N - 100);
+        int M = Integer.parseInt(br.readLine());
+        if (M > 0) { 
+            String[] nbtn = br.readLine().split(" ");
+            for (int i = 0; i < M; i++) {
+                btn[Integer.parseInt(nbtn[i])] = true; 
+            }
+        }
 
-		boolean[] btn = new boolean[10];
+        for (int i = 0; i <= 999999; i++) {
+            String current = Integer.toString(i);
+            boolean isValid = true;
 
-		int M = Integer.parseInt(br.readLine());
+            for (int j = 0; j < current.length(); j++) {
+                int digit = current.charAt(j) - '0';
+                if (btn[digit]) { 
+                    isValid = false;
+                    break;
+                }
+            }
 
-		if (M > 0) {
-			String[] nbtn = br.readLine().split(" ");
-			for (int i = 0; i < M; i++) {
-				btn[Integer.parseInt(nbtn[i])] = true;
-			}
-		}
+            if (isValid) {
+                result = Math.min(result,  Math.abs(i - N) + current.length());
+            }
+        }
 
-		for (int i = 0; i <= 999999; i++) {
-			String[] check = Integer.toString(i).split("");
-			boolean isValid = true;
-
-			for (int j = 0; j < check.length; j++) {
-				if (btn[Integer.parseInt(check[j])]) {
-					isValid = false;
-					break;
-				}
-			}
-
-			if (isValid) {
-				result = Math.min(result, Math.abs(i - N) + check.length);
-			}
-		}
-
-		sb.append(result);
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
+        sb.append(result);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
