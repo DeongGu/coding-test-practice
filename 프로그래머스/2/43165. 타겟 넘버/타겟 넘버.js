b121eb05af077314e0b6964e611da6eac94fc71c
SeongWoo Shin <ssw4510@gmail.com>
2025-05-12 16:14:53 +0900

    [level 2] Title: 타겟 넘버, Time: 21.28 ms, Memory: 35.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,21 @@
+function solution(numbers, target) {
+    
+    let result = 0;
+    
+    dfs(0,0);
+    
+    function dfs(idx,sum){
+        if(idx === numbers.length){
+            if(sum === target){
+                result+=1;
+            }
+            return;
+        }
+        
+        dfs(idx+1, sum + numbers[idx]);
+        dfs(idx+1, sum - numbers[idx]);
+        
+    }
+    
+    return result;
+}
