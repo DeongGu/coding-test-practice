c50e306b61c6879e5105622162587cd3227b558f
SeongWoo Shin <ssw4510@gmail.com>
2025-09-06 19:59:30 +0900

    [level 2] Title: 교점에 별 만들기, Time: 421.42 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.py
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.py
@@ -1,7 +1,6 @@
 import math
 
 def solution(line):
-
     dots = set()
     n = len(line)
     min_x, min_y = float("inf"), float("inf")
@@ -9,38 +8,25 @@ def solution(line):
     
     for i in range(n):
         a, b, e = line[i]
-
         for j in range(i + 1, n):
             c, d, f = line[j]
-            if a * d == b * c: continue
+            if a * d == b * c:
+                continue  # 평행
             x = (b * f - e * d) / (a * d - b * c)
             y = (e * c - a * f) / (a * d - b * c)
             
             if x == math.trunc(x) and y == math.trunc(y):
-                dots.add((int(x),int(y)))
-                
-                if x > max_x: max_x = x
-                if y > max_y: max_y = y
-                if x < min_x: min_x = x
-                if y < min_y: min_y = y
+                x, y = int(x), int(y)
+                dots.add((x, y))
+                min_x, max_x = min(min_x, x), max(max_x, x)
+                min_y, max_y = min(min_y, y), max(max_y, y)
 
-        
-    r = int(max_y - min_y) + 1
-    c = int(max_x - min_x) + 1
+    r = max_y - min_y + 1
+    c = max_x - min_x + 1
 
     result = [["."] * c for _ in range(r)]
-    answer = []
+    for x, y in dots:
+        result[y - min_y][x - min_x] = "*"
     
-    for dot in dots:
-        moved_dot_x = dot[0] - int(min_x)
-        moved_dot_y = dot[1] - int(min_y)
-        result[moved_dot_y][moved_dot_x] = "*"
-    
-    for el in result:
-        answer.append("".join(el))
-    
-    answer.reverse()
-    
+    answer = ["".join(row) for row in reversed(result)]
     return answer
-
-            
