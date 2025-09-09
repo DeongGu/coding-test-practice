e136189ec953e1ae604fafe6d6f8fc746ed7223b
SeongWoo Shin <ssw4510@gmail.com>
2025-09-09 19:32:17 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 169.81 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -12,29 +12,30 @@ def solution(rows, columns, queries):
         
         result = []
         
-        new_matrix = [row[:] for row in matrix]
+        tmp = matrix[r1][c1]
+        result.append(tmp)
         
-#         r1 일때 c1 ~ c2
-        for c in range(c1+1, c2+1):
-            new_matrix[r1][c] = matrix[r1][c-1]
-            result.append(new_matrix[r1][c])
+#         c1 일때 r1 ~ r2
+        for r in range(r1, r2):
+            matrix[r][c1] = matrix[r+1][c1]
+            result.append(matrix[r][c1])
+        
+#         r2 일때 c1 ~ c2
+        for c in range(c1, c2):
+            matrix[r2][c] = matrix[r2][c+1]
+            result.append(matrix[r2][c])
             
-#         c2 일때 r1 ~ r2
-        for r in range(r1+1, r2+1):
-            new_matrix[r][c2] = matrix[r-1][c2]
-            result.append(new_matrix[r][c2])
+#         c2 일때 r2 ~ r1
+        for r in range(r2, r1, -1):
+            matrix[r][c2] = matrix[r-1][c2]
+            result.append(matrix[r][c2])
         
-#         r2 일때 c2 ~ c1
-        for c in range(c2 - 1, c1 - 1 , -1):
-            new_matrix[r2][c] = matrix[r2][c+1]
-            result.append(new_matrix[r2][c])
-        
-#         c1 일때 r2 ~ r1
-        for r in range(r2 - 1, r1 - 1 , -1):
-            new_matrix[r][c1] = matrix[r+1][c1]
-            result.append(new_matrix[r][c1])
-        
+#         r1 일때 c2 ~ c1
+        for c in range(c2 , c1 + 1 , -1):
+            matrix[r1][c] = matrix[r1][c-1]
+            result.append(matrix[r1][c])
+    
+        matrix[r1][c1+1] = tmp
         answer.append(min(result))
-        matrix = new_matrix
     
     return answer
