2021f189e79f9eed8239b0dfba4fe62be983c9f5
SeongWoo Shin <ssw4510@gmail.com>
2025-09-11 20:56:31 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.48 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,19 +1,11 @@
 def solution(s):
-    answer = []
-    c_cnt = 0
-    z_cnt = 0
+    c_cnt, z_cnt = 0, 0
     
     while s != "1":
         c_cnt += 1
-        before_length = len(s)
-        s = s.replace("0", "")
-        z_cnt += before_length - len(s)
-        tmp = len(s)
-        nums = []
-        while tmp:
-            tmp, digit = divmod(tmp, 2)
-            nums.append(str(digit))
+        cnt = s.count("1")
+        z_cnt += len(s) - cnt
         
-        s = "".join(nums)
+        s = bin(cnt)[2:]
     
     return [c_cnt, z_cnt]
