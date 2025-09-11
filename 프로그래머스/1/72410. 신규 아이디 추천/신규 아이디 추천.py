c648bbe48fc71fe2ba6e9e5c54d7aeecbaa64488
SeongWoo Shin <ssw4510@gmail.com>
2025-09-11 21:30:36 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,27 @@
+def solution(new_id):
+    
+    answer = new_id.lower()
+    
+    filtered = []
+    for c in answer:
+        if c.isalpha() or c.isdigit() or c in ["-", "_", "."]:
+            filtered.append(c)
+    
+    answer = "".join(filtered)
+    
+    while ".." in answer:
+        answer = answer.replace("..", ".")
+    
+    answer = answer.strip(".")
+    
+    if answer == "": answer = "a"
+    
+    if len(answer) >= 16:
+        answer = answer[:15]
+    if answer[-1] == ".": answer = answer[:-1]
+    
+    if len(answer) <= 2:
+        while len(answer) < 3:
+            answer += answer[-1]
+    
+    return answer
