680b14277597a76c66c2a869da1e9919de47f703
SeongWoo Shin <ssw4510@gmail.com>
2025-09-12 17:15:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.47 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,14 +1,12 @@
-def solution(num):
-
-    def recursion(n, cnt):
+def recursion(n, cnt):
         
         if n == 1: return cnt
-        if cnt == 500 and n != 1: return -1
+        if cnt == 500: return -1
         
-        if n % 2 == 0: n /= 2
-        else:
-            n = n * 3 + 1
+        if n % 2 == 0: 
+            return recursion(n // 2, cnt + 1)
+        elif n % 2 == 1:
+            return recursion(n * 3 + 1, cnt + 1)
         
-        return recursion(n, cnt+1)
-        
+def solution(num):
     return recursion(num, 0)
