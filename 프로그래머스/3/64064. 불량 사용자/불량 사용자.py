e78c7a6cc1ea8aa92a4a5c5d55f3adcccd558c80
SeongWoo Shin <ssw4510@gmail.com>
2025-09-15 21:36:09 +0900

    [level 3] Title: 불량 사용자, Time: 167.20 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,14 +1,24 @@
-from itertools import permutations
 import re
-        
+
+def dfs(ban_idx, banned, users, visited, picked, answer):
+    if ban_idx == len(banned):
+        answer.add(tuple(sorted(picked)))
+        return
+    
+    for i in range(len(users)):
+        if visited[i] or not re.fullmatch(banned[ban_idx], users[i]):
+            continue
+
+        visited[i] = True
+        dfs(ban_idx+1, banned, users, visited, picked+[users[i]], answer)
+        visited[i] = False
+
 def solution(user_id, banned_id):
     answer = set()
     
-    ban = " ".join(banned_id).replace("*",".")
+    banned = [x.replace("*", ".") for x in banned_id]
     
-    for i in permutations(user_id, len(banned_id)):
-        
-        if re.fullmatch(ban, " ".join(i)):
-            answer.add(" ".join(sorted(i)))
+    visited = [False] * len(user_id)
+    dfs(0, banned, user_id, visited, [], answer)
     
     return len(answer)
