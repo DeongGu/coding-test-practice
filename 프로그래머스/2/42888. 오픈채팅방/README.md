d4ab6d0e52811ed4d505cd988e15814b3672eede
SeongWoo Shin <ssw4510@gmail.com>
2025-09-23 23:08:01 +0900

    [level 2] Title: 오픈채팅방, Time: 152.45 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888) 
 
 ### 성능 요약
 
-메모리: 45.6 MB, 시간: 160.33 ms
+메모리: 63.2 MB, 시간: 152.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 23일 19:44:32
+2025년 09월 23일 23:07:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -4,24 +4,20 @@ def solution(record):
     result = []
     
     for info in record:
-        parts = info.split(" ")
+        parts = info.split()
         order = parts[0]
         uid = parts[1]
-        nickname = parts[2] if len(parts) > 2 else None
         
-        if order == "Enter":
-            data[uid] = [nickname, 1]
-            history.append([uid,1])
-        elif order == "Change":
-            data[uid][0] = nickname
-        else:
-            data[uid][1] = 0
-            history.append([uid,0])
+        if order in ["Enter", "Change"]:
+            nickname = parts[2]
+            data[uid] = nickname
             
-    for uid, isStatus in history:
-        if isStatus == 1:
-            result.append("".join([data[uid][0],"님이 들어왔습니다."]))
-        else:
-            result.append("".join([data[uid][0],"님이 나갔습니다."]))
+        history.append((order, uid))
+            
+    for order, uid in history:
+        if order == "Enter":
+            result.append("".join([data[uid],"님이 들어왔습니다."]))
+        elif order == "Leave":
+            result.append("".join([data[uid],"님이 나갔습니다."]))
     
     return result
