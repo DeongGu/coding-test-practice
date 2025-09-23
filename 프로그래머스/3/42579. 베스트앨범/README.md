732a15de4f361c7f11843042ac30922950c0941c
SeongWoo Shin <ssw4510@gmail.com>
2025-09-23 23:27:41 +0900

    [level 3] Title: 베스트앨범, Time: 0.09 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 0.12 ms
+메모리: 9.25 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 23일 20:20:05
+2025년 09월 23일 23:27:37
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,28 +1,18 @@
 def solution(genres, plays):
     answer = []
-    
     play_data = {}
     data = {}
     
-    for idx, genre in enumerate(genres):
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
         if genre in data:
-            data[genre].append([idx, plays[idx]])
-            data[genre].sort(key=lambda x: x[1], reverse=True)
-            play_data[genre] += plays[idx]
+            data[genre].append((idx, play))
         else:
-            data[genre] = [[idx, plays[idx]]]
-            play_data[genre] = plays[idx]
-    
-
-    sum_data = [[key, play_data[key]] for key in play_data]
-    sum_data.sort(key=lambda x : x[1], reverse=True)
-    
-    for genre, value in sum_data:
+            data[genre] = [(idx, play)]
         
-        if len(data[genre]) >= 2:
-            answer.append(data[genre][0][0])
-            answer.append(data[genre][1][0])
-        else:
-            answer.append(data[genre][0][0])
+        play_data[genre] = play_data.get(genre, 0) + play
+
+    for (genre, _) in sorted(play_data.items(), key=lambda x: x[1], reverse=True):
+        for (idx, _) in sorted(data[genre], key=lambda x: x[1], reverse=True)[:2]:
+            answer.append(idx)
     
     return answer
